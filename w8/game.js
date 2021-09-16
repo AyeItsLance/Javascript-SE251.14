dc6ef2b4bb2ed0c69070cc93350936c325636b45
lance lespinasse <buttonmasher123@yahoo.com>
2021-09-16 04:14:34 -0400

    Final Update

--- w8/game.js
+++ w8/game.js
@@ -1,8 +1,5 @@
 var c = document.querySelector(`canvas`)
 var ctx = c.getContext(`2d`)
-
-
-
 var timer = requestAnimationFrame(main)
 
 class Car
@@ -17,7 +14,6 @@ class Car
         this.vy=0
         this.color=`hotpink`
     }
-
     draw()
     {
         ctx.fillStyle=this.color
@@ -32,62 +28,65 @@ class Car
 
 car = []
 
-var currentState = 'race'
 
+
+var currentState = `race`
 var states=[]
-
-states['start'] = function(){
-    
+states[`start`]=function(){
     var y = 100
-    for(let i=0; i<3; i++)
+    for(let i=0; i<2; i++)
     {
-
-        car[i] = new Car()
-
-        car[i].y=y
+        car[i]=new Car()
+        car[i].y=y;
         y+=150
-
         car[i].vx = Math.round(Math.random()*(10-5)+5)
-
         var r = Math.round(Math.random()*(255-0)+0)
         var g = Math.round(Math.random()*(255-0)+0)
         var b = Math.round(Math.random()*(255-0)+0)
-
         car[i].color = `rgb(${r},${g},${b})`
     }
-
-
+    
+    var line = new Car()
+    line.color = `black`
+    line.x = 900
+    line.y = 0
+    line.w = 10
+    line.h = c.height
 }
 
+states[`race`]=function(){
+    line.draw()
+    
+    for(i in car)
+    {
+        car[i].move()
+        
 
-
-var line = new Car()
-line.color = 'black'
-line.x = 900
-line.y = 0
-line.w = 10
-linw.h = c.height
-
-
-
+        if(car[i].x+car[i].w >= line.x)
+        {
+          //ctx.font =`30px Arial`
+          //ctx.fillText(`Car ${car.indexOf(car[i])} Wins!`, 20, 30)  
+          currentState=`win`
+        }
+        
+        car[i].draw()
+    }
+}
+states[`win`]=function(){
+    line.draw()
+    
+    for(i in car)
+    {     
+        car[i].draw()
+    }
+}
 
 
 function main()
 {
     ctx.clearRect(0,0, c.width, c.height);
+    
+    states[currentState]()
 
-    line.draw()
-
-    for(i in car)
-    {
-        car[i].move()
-        car[i].draw()
-
-        if(car[i].x+car[i].w >= line.x)
-        {
-            ctx.font = `30px Arial`
-            ctx.fillText(`Car ${car.indexOf(car[i])} Wins`, 20, 30)
-        }
-    }
     timer = requestAnimationFrame(main)
 }
